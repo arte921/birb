1ec6f4b61762a6f41556a7718cf20c434c546acf
Ard-Jan van Etten <ardjanve@gmail.com>
2022-01-19 08:41:08 +0100

    e

--- birb.js
+++ birb.js
@@ -1,5 +1,6 @@
 const zichtafstand = 20;
 const dimensies = 2;
+const streefafstand = 2;
 
 const boids = [
     {
@@ -16,47 +17,39 @@ const boids = [
     }
 ];
 
-console.log([1, 2, 3][1]);
-
-const magic = array => {
-    if (array[0]) {
-        for (const key of Object.keys(array[0])) {
-            Object.defineProperty(array, key, {
-                get: () => array.map((entry) => entry[key])
-            });
-        }
-    }
-    return array;
-};
-
 const it = (aantal) => Array.from(Array(aantal)).map((_, i) => i);
 const totaal = array => array.reduce((a, b) => a + b);
 
 const dimensielijst = it(dimensies);
 
+const lengte = vector => Math.sqrt(totaal(vector.map(c => c ** 2)));
+
 const afstand = (a, b) => Math.sqrt(totaal(dimensielijst.map(d => (a[d] - b[d]) ** 2)));
+
+const metLengte = (vector, lengte, x = Math.sqrt(lengte ** 2 / totaal(vector.map(c => c ** 2)))) => vector.map(c => c * x);
+
+console.log((metLengte([3, 4], 12)));
 
 const gemiddelde = vectoren => vectoren
     .reduce((a, b) => a.map((a, d) => a + b[d]))
     .map(coordinaat => coordinaat / boids.length);
 
-const zichtbareBoids = (boid, boids, radius = zichtafstand) => boids.filter(mate => afstand(mate, boid) <= radius && mate != boid);
+const zichtbareBoids = (boid, boids, radius = zichtafstand) => boids.filter(mate => afstand(mate.pos, boid.pos) <= radius && mate != boid);
 
 const boidsMetStats = (boid, boids) => ({
     mates: boids.map(mate => ({
         ...mate,
-        afstand: afstand(boid, mate)
+        afstand: afstand(boid.pos, mate.pos),
+        absoluteSnelheid: lengte(boid.snelheid)
     })),
-    centrum: gemiddelde(boids.pos),
-    richting: gemiddelde(boids.snelheid)
+    centrum: gemiddelde(boids.map(b => b.pos)),
+    richting: gemiddelde(boids.map(b => b.snelheid))
 });
 
 const step = boids => boids.map((boid, _, boids) => {
-    const zichtbareMates = zichtbareBoids(boid, boids);
+    const zichtbareMates = boidsMetStats(boid, zichtbareBoids(boid, boids));
 
     console.log(zichtbareMates);
-
-
 });
 
 step(boids);
