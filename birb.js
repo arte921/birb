af7ee54d3345d8f06ec2e54a882709a4b3c29454
Ard-Jan van Etten <ardjanve@gmail.com>
2022-01-19 08:08:57 +0100

    e

--- birb.js
+++ birb.js
@@ -1,41 +1,62 @@
-const radius = 20;
+const zichtafstand = 20;
+const dimensies = 2;
 
 const boids = [
     {
-        x: 10,
-        y: 10,
-        r: 2,
-        v: 10
+        pos: [10, 10],
+        snelheid: [2, 3]
     },
     {
-        x: 20,
-        y: 10,
-        r: 1.5,
-        v: 10
+        pos: [3, 4],
+        snelheid: [3, 2]
     },
     {
-        x: 5,
-        y: 10,
-        r: 1,
-        v: 10
+        pos: [5, 2],
+        snelheid: [4, 1]
     }
 ];
 
-const afstand = (a, b) => Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+console.log([1, 2, 3][1]);
 
-const zichtbareBoids = (boid, boids, radius = radius) => boids.filter(mate => afstand(mate, boid) <= radius && mate != boid);
+const magic = array => {
+    if (array[0]) {
+        for (const key of Object.keys(array[0])) {
+            Object.defineProperty(array, key, {
+                get: () => array.map((entry) => entry[key])
+            });
+        }
+    }
+    return array;
+};
+
+const it = (aantal) => Array.from(Array(aantal)).map((_, i) => i);
+const totaal = array => array.reduce((a, b) => a + b);
+
+const dimensielijst = it(dimensies);
+
+const afstand = (a, b) => Math.sqrt(totaal(dimensielijst.map(d => (a[d] - b[d]) ** 2)));
+
+const gemiddelde = vectoren => vectoren
+    .reduce((a, b) => a.map((a, d) => a + b[d]))
+    .map(coordinaat => coordinaat / boids.length);
+
+const zichtbareBoids = (boid, boids, radius = zichtafstand) => boids.filter(mate => afstand(mate, boid) <= radius && mate != boid);
 
 const boidsMetStats = (boid, boids) => ({
     mates: boids.map(mate => ({
         ...mate,
         afstand: afstand(boid, mate)
     })),
-    centrumX: boids.reduce((a, b) => a.x + b.x) / boids.length,
-    centrumY: boids.reduce((a, b) => a.y + b.y) / boids.length
+    centrum: gemiddelde(boids.pos),
+    richting: gemiddelde(boids.snelheid)
 });
 
 const step = boids => boids.map((boid, _, boids) => {
     const zichtbareMates = zichtbareBoids(boid, boids);
 
+    console.log(zichtbareMates);
+
 
 });
+
+step(boids);
